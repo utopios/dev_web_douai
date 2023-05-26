38c07b4ebbc3b54a73263485960d3d8917a34a63
Christophe-Ringot <christophe.ringot1996@gmail.com>
2023-05-26 09:05:39 +0200

    demo if-else sujet exo 10 à 12

---
+++ Javascript/01-Cours/01-DecouverteLangage/04-LesStructuresConditionelles/main.js
@@ -0,0 +1,83 @@
+/*
+    Les structures conditionnelles
+*/
+
+console.log("Les structures conditionnelles");
+
+/*
+    if(condition) {
+        si la condition est vrai => Executer le code.
+    } else {
+        si la condition est fausse => Execute le code.
+    }
+*/
+
+// Exemple 
+
+let compte = 3000;
+
+if(compte > 0) {
+    console.log("Votre compte est créditeur !");
+} else {
+    console.log("Votre compte est débiteur !");
+}
+
+// Avec plusieurs instructions
+
+if(compte > 0) {
+    console.log("Votre compte est créditeur !");
+    console.log(`Vous avez ${compte} €`);
+} else {
+    console.log("Votre compte est débiteur !");
+    console.log(`Vous avez ${compte} €`);
+}
+
+// Avec un booléen
+
+let estCrediteur = compte > 0
+
+if (estCrediteur) {
+    console.log("Votre compte est créditeur ! (avec booléen)");
+} else {
+    console.log("Votre compte est débiteur ! (avec booléen)");
+}
+
+// Avec une récupération de saisie utilisateur
+
+// let age = Number(prompt("Indiquer votre age "))
+
+// if (age >= 18) {
+//     console.log("Vous êtes majeur !");
+// } else {
+//     console.log("Vous êtes mineur !");
+// }
+
+/*
+    Avec Sinon Si
+*/
+
+// Structures conditionnelle avec vérification multiples
+
+if (compte > 0 )
+{
+    console.log("Votre compte est créditeur !");
+    console.log(`Vous avez ${compte} €`);
+} else if (compte === 0) {
+    console.log("Votre compte est null ");
+    console.log(`Vous avez ${compte} €`);
+} else {
+    console.log("Votre compte est débiteur !");
+    console.log(`Vous avez ${compte} €`);
+}
+
+/*
+    Les opérateurs de comparaison.
+     - == signifie égal à / Permet de vérifier si deux valeurs sont identiques
+     - === signifie égal à / Permet de vérifier si deux valeurs et types sont identiques
+     - != signifie est différent de / Permet de vérifier si deux valeurs sont différentes
+     - !== signifie est différent de / Permet de vérifier si deux valeurs et types sont différentes
+*/
+
+console.log(1 == 1); // vrai
+console.log(1 === '1'); // Faux
+console.log(1 == '1');// vrai
