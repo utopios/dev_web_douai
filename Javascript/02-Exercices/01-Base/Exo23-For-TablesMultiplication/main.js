6c2d85f58096d76fed39575c4f8399b8749f2fe5
Christophe-Ringot <christophe.ringot1996@gmail.com>
2023-05-31 09:03:42 +0200

    correction exo 23 et ajout exo 24 et 25

--- Javascript/02-Exercices/01-Base/Exo23-For-TablesMultiplication/main.js
+++ Javascript/02-Exercices/01-Base/Exo23-For-TablesMultiplication/main.js
@@ -1,3 +1,17 @@
 /**
  * EXERCICE23 - Boucle For - Tables de multiplication
  */
+const result = document.querySelector('.result')
+
+//Déclaration variables
+let affichage = `<h2>Tables de multiplication</h2>`
+
+for(let i = 1; i <= 10; i++){
+    affichage += `<div class="card"><h5>Table de <b>${i}</b></h5><ul>`
+    for(let j = 1; j <= 10; j++){
+        affichage += `<li class="tab-25">${i} x ${j} = ${i*j}</li>`
+    }
+    affichage += `</ul></div>`
+}
+
+result.innerHTML = affichage
