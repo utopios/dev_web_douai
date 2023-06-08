da9a17846dbf73b94b0e99482cdbdca51e9a5909
Christophe-Ringot <christophe.ringot1996@gmail.com>
2023-06-08 16:14:07 +0200

    debut correction tp3

--- Javascript/02-Exercices/02-POO/Tp-POO-ParkingJs/module/index.js
+++ Javascript/02-Exercices/02-POO/Tp-POO-ParkingJs/module/index.js
@@ -3,15 +3,23 @@
 // Création du tableau parking
 
 // Stockage du DOM dans des variables
-
+const btnPayer = document.querySelector("#btnPayer")
+const btnEnter = document.querySelector('#btnEnter')
+const licencePlate = document.querySelector('#licencePlate')
+const successBox = document.querySelector('#successBox')
+const alertBox = document.querySelector('#alertBox')
+const messageBox = document.querySelector('#messageBox')
 
 // fonction onclick avec le bouton btnEnter
 
-
 // fonction onclick avec le bouton btnPayer
 
-function displayBox() {
-    // Fonction pour l'affichage des messages
+function displayBox(domElement, message, delay) {
+    domElement.style.display = "block"
+    domElement.textContent = message
+    setTimeout(() => {
+        domElement.style.display = "none"
+    },delay)
 }
 
 function addToPark () {
@@ -24,5 +32,13 @@ function findCar() {
 
 function init(){
     // Ajouter des véhicules à la liste
-    new Vehicule('AA-123-AA', new Date("2023-06-08T14:00:00"))
+    // new Vehicule('AA-123-AA', new Date("2023-06-08T14:00:00"))
+
+    alertBox.style.display = "none"
+    messageBox.style.display = "none"
+    successBox.style.display = "none"
+
+   // displayBox(successBox, "Veuillez prendre votre ticket", 5000)
 }
+
+window.onload = init()
