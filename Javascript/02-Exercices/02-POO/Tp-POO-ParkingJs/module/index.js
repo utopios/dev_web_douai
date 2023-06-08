929784963f5eee59672f857980a74dd8def7a8f6
Christophe-Ringot <christophe.ringot1996@gmail.com>
2023-06-08 13:53:17 +0200

    début structure du tp

---
+++ Javascript/02-Exercices/02-POO/Tp-POO-ParkingJs/module/index.js
@@ -0,0 +1,27 @@
+// Import de la classe véhicule
+
+// Création du tableau parking
+
+// Stockage du DOM dans des variables
+
+
+// fonction onclick avec le bouton btnEnter
+
+
+// fonction onclick avec le bouton btnPayer
+
+function displayBox() {
+    // Fonction pour l'affichage des messages
+}
+
+function addToPark () {
+    // Fonction pour ajouter une voiture dans le parking
+}
+
+function findCar() {
+    // Fonction pour chercher si un véhicule existe dans le parking
+}
+
+function init(){
+    // Ajouter des véhicules à la liste
+}
